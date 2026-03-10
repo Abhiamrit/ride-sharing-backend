4997b7f13d904a52c0d492fc46d74986dc599f11
Abhiamrit <abhiamritveera@gmail.com>
2026-03-10 16:56:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// root route
+app.get("/", (req, res) => {
+  res.send("Ride Sharing Backend API is running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/rides", rideRoutes);
 
-// Use dynamic port for deployment
+// dynamic port for deployment
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Ride Sharing Backend API is running");
 });
